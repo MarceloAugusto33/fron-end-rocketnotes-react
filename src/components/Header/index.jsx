20bb0b9ba2a08153e6ab40b2b1794abfdf02148c
marcelo <sjsjsjmarcelo@gmail.com>
2024-01-13 19:54:54 -0300

    Created component Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import { Container, Profile, Logout } from "../Header/styles"
+import { RiShutDownLine } from 'react-icons/ri'
+
+export function Header() {
+    return (
+        <Container>
+            <Profile>
+                <img
+                    src="https://github.com/marceloaugusto33.png" 
+                    alt="Foto do Usuario"
+                />
+
+                <div>
+                    <span>Bem vindo!</span>
+                    <strong>Marcelo Augusto Tavares</strong>
+                </div>
+            </Profile>
+
+
+            <Logout>
+                <RiShutDownLine/>
+            </Logout>
+
+        </Container>
+    )
+}
