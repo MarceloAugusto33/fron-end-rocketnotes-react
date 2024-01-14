18eedb1a7aedcd8d72422666c7e19e847af3afaa
marcelo <sjsjsjmarcelo@gmail.com>
2024-01-14 18:51:21 -0300

    created page NewNote

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,47 @@
+import { Container, Form } from './styles';
+
+//Imports Components
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
+import { Section } from '../../components/Section';
+import { NoteItem } from '../../components/NoteItem';
+import { Button } from '../../components/Button';
+
+export function New() {
+    return (
+        <Container>
+            <Header />
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <a href="/">Voltar</a>
+                    </header>
+
+                    <Input
+                        placeholder="Titulo"
+                    />
+
+                    <Textarea placeholder="Observações" />
+
+                    <Section title="Links úteis">
+                        <NoteItem value="https://rocketseat.com.br" />
+                        <NoteItem isNew placeholder='Novo link' />
+                    </Section>
+
+                    <Section title="Marcadores">
+                        <div className='tags'>
+                            <NoteItem value="react" />
+                            <NoteItem isNew placeholder='Nova Tag' />
+
+                        </div>
+                    </Section>
+
+                    <Button title="Salvar" />
+                </Form>
+            </main>
+        </Container>
+    )
+}
