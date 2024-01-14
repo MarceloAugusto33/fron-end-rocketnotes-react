90bf1a55a0a4f572cc7f7136135008bf103aa4c9
marcelo <sjsjsjmarcelo@gmail.com>
2024-01-14 15:01:40 -0300

    Crated page Home

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import { ThemeProvider } from 'styled-components';
 import { Details } from './pages/Details';
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
+import { Home } from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,42 @@
-import { Container } from "./styles";
+import { Container, Links, Content } from "./styles";
 
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/tag";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
   return (
     <Container>
       <Header />
-      
-      <Button title="Voltar"/>
+
+
+      <main>
+        <Content>
+
+
+
+          <ButtonText title="Excluir nota" />
+
+          <h1>Introdução ao React</h1>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum et fugit ea saepe. Aliquam adipisci reprehenderit, alias dolorum atque sapiente vitae mollitia officiis unde molestiae impedit illum culpa doloremque aut?</p>
+
+          <Section title="Links uteis">
+            <Links>
+              <li><a href="#">www.rocketseat.com.br</a></li>
+              <li><a href="#">www.rocketseat.com.br</a></li>
+            </Links>
+          </Section>
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
 
   )
